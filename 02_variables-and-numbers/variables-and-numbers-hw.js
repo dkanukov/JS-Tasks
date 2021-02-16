9cca2ad7e0808274d9a0239c31a88801f0d3f392
denis <a1@MacBook-Pro-denis.local>
2021-02-16 22:46:48 +0300

    correction

--- 02_variables-and-numbers/variables-and-numbers-hw.js
+++ 02_variables-and-numbers/variables-and-numbers-hw.js
@@ -1,54 +1,60 @@
-// Задание 1
+// // Задание 1
 
-let x1 = 8;
-let y1 = 1;
-let x2 = 5;
-let y2 = 1;
-let s = (x2 - x1) * (y2 - y1);
-s = Math.abs(s);
-console.log(s);
-
-
-//Задание 2
-
-let a = 13.890123;
-let b = 2.891564;
-let n = 3;
-
-a1 = Math.trunc(a);
-b1 = Math.trunc(b);
-
-a = a - Math.trunc(a);
-b = b - Math.trunc(b);
-
-a = a * Math.pow(10, n);
-b = b * Math.pow(10, n);
-
-a = Math.trunc(a);
-b = Math.trunc(b);
-
-console.log(a);
-console.log(b);
-console.log("a = b", a == b);
-console.log("a != b", a != b);
-console.log("a > b", a > b);
-console.log("a < b", a < b);
-console.log("a >= b", a >= b);
-console.log("a <= b", a <= b);
+// let x1 = 8;
+// let y1 = 1;
+// let x2 = 5;
+// let y2 = 1;
+// let s = (x2 - x1) * (y2 - y1);
+// s = Math.abs(s);
+// console.log(s);
 
 
-// Задание 3 
+// //Задание 2
 
-let n = 100;
-let m = -5;
+// let a = 13.890123;
+// let b = 2.891564;
+// let n = 3;
+
+// a1 = Math.trunc(a);
+// b1 = Math.trunc(b);
+
+// a = a - Math.trunc(a);
+// b = b - Math.trunc(b);
+
+// a = a * Math.pow(10, n);
+// b = b * Math.pow(10, n);
+
+// a = Math.trunc(a);
+// b = Math.trunc(b);
+
+// console.log(a);
+// console.log(b);
+// console.log("a = b", a == b);
+// console.log("a != b", a != b);
+// console.log("a > b", a > b);
+// console.log("a < b", a < b);
+// console.log("a >= b", a >= b);
+// console.log("a <= b", a <= b);
+
+
+// // Задание 3 
+
+let n = 1;
+let m = 3;
 
 let range = Math.abs(m - n);
 let min = Math.min(n, m);
 let numberinrange = Math.round(Math.random() * range) + min;
+let max = Math.max(n,m)
 
 if (numberinrange % 2 == 1) {
     console.log(numberinrange);
 } else{
     numberinrange += 1;
-    console.log(numberinrange);
+    if (numberinrange <= max) {
+        console.log(numberinrange);
+    } else{
+        numberinrange -= 2;
+        console.log(numberinrange);
+    }
 }

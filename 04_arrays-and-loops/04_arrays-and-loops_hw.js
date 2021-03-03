29d6bdeb9c1291273e511134a7c870ab53b15ce2
Denis Kanukov <deniskanukov@MacBook-Pro-Denis.local>
2021-03-04 02:12:45 +0300

    Внесение правок

--- 04_arrays-and-loops/04_arrays-and-loops_hw.js
+++ 04_arrays-and-loops/04_arrays-and-loops_hw.js
@@ -1,70 +1,59 @@
 // Задание 1 
-let i = 0
-let count = 70
-let n = 100
-let m = -5
-let arr = []
+let i = 0;
+let count = 70;
+let n = 100;
+let m = -5;
+let arr = [];
 
 while(i < count){
-    let range = Math.abs(m - n)
-    let min = Math.min(n, m) 
-    let numberinrange = Math.round(Math.random() * range) + min
-    let max = Math.max(n,m)
-    arr[i] = numberinrange
-    i = i + 1
+    let range = Math.abs(m - n);
+    let min = Math.min(n, m);
+    let numberinrange = Math.round(Math.random() * range) + min;
+    let max = Math.max(n,m);
+    arr[i] = numberinrange;
+    i = i + 1;
 }
 
-console.log(arr)
+console.log(arr);
 
 // Задание 2
-let string = "Привет, мир!"
-let string_reverse = ""
-let arr = []
-for (let i = 0; i < string.length; i++) {
-    arr[i] = string[i];
-}
-
-for(let i = string.length - 1; i >= 0; i--){
-    string_reverse = string_reverse + arr[i]
-}
-console.log(string_reverse)
-
+let string_reverse = string.split('').reverse().join('');
 
 // Задание 3
 
-roadMines = [true, false, false, false, false, false, false, false, false, true]
-let health = 2
+roadMines = [true, false, false, false, false, false, false, false, false, true];
+let health = 2;
 for(let i = 0; i <= roadMines.length; i++){
-    let k = i + 1
-    if (roadMines[i] == true) {
-        console.log("танк переместился на", k)
-        health = health - 1
-        if (health == 0) {
-            console.log("Танк уничтожен")
-            break
+    let k = i + 1;
+    if (roadMines[i]) {
+        console.log('танк переместился на', k);
+        health = health - 1;
+        if (health === 0) {
+            console.log('Танк уничтожен');
+            break;
         }else{
-            console.log("Танк повреждён")
+            console.log('Танк повреждён');
         }
     } else{
-        console.log("танк переместился на", k)
+        console.log('танк переместился на', k);
     }
 }
 
 // Задание 4
 
-let day = []
-let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
-let k = 0
+let day = [];
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let k = 0;
 
 for(let i = 0; i <= 30; i++){
-    day.push(i+1)
+    day.push(i+1);
 }
 
 for(let i = 0; i <= 30; i++){
     if (k !== 7) {
-        console.log(day[i], "января", week[k])
-        k += 1
+        console.log(day[i], 'января', week[k]);
+        k += 1;
     }else{
-        k = 0
+        k = 0;
     }
 }

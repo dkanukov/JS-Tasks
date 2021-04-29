9516f257ef6d62197b32cd7676304f83bc3738b8
Denis <48333782+dkanukov@users.noreply.github.com>
2021-04-29 19:04:06 +0300

    1

--- 05_functions/filter-black-list.js
+++ 05_functions/filter-black-list.js
@@ -1,17 +1,20 @@
-function outputWhiteListEmails(whiteL, blackL){
-    for (let index = 0; index < blackL.length; index++) {
-        nameInBlackList = blackL[index];
-        curNameInWhiteList = whiteL.indexOf(nameInBlackList);
-        if(curNameInWhiteList != -1){
-            whiteL.splice(curNameInWhiteList, curNameInWhiteList+1);
+function outputWhiteListEmails(whiteList, blackList, sortedList){
+    let n;
+    for (let index = 0; index < whiteList.length; index++) {
+        n = blackList.includes(whiteList[index]);
+        if (n == false) {
+            sortedList.push(whiteList[index])
         }
     }
-    return whiteL
+    return sortedList
 }
+
+
+var sortedList = [];
 var whiteList = ['qw@gmail.com', 'qweqew@gmail.com', 'asdasda@gmail.com', 'zczxczcz@gmail.com',
  '123@gmail.com', '4353453@gmail.com'];
-var blacklist = ['123@gmail.com', '4353453@gmail.com'];
-newWhiteList = outputWhiteListEmails(whiteList, blacklist);
-console.log(newWhiteList);
+var blackList = ['123@gmail.com', '4353453@gmail.com'];
+outputWhiteListEmails(whiteList, blackList, sortedList);
+console.log(sortedList);
 
-default {outputWhiteListEmails};
+export default outputWhiteListEmails;

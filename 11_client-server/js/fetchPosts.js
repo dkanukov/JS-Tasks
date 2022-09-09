370c55c4252282853c8f7f147fd6abe14f3fae22
Denis <kanukov.denis@gmail.com>
2022-09-09 20:39:09 +0300

    upd posts rendering

--- 11_client-server/js/fetchPosts.js
+++ 11_client-server/js/fetchPosts.js
@@ -1,10 +1,37 @@
-async function getPosts() {
-  const response = await fetch('https://gorest.co.in/public/v2/posts');
-  const posts = await response.json();
-  return posts;
+const contentPlace = document.getElementById('content');
+
+function createPosts(post) {
+  const link = document.createElement('a');
+  const card = document.createElement('div');
+  const cardBody = document.createElement('div');
+  const cardText = document.createElement('p');
+  link.href = `post.html?postId=${post.id}`;
+  link.classList.add('post-link');
+  card.classList.add('card');
+  cardBody.classList.add('card-body');
+  cardText.classList.add('text-center', 'text-dark');
+  cardText.innerText = post.title;
+
+  cardBody.append(cardText);
+  card.append(cardBody);
+  link.append(card);
+
+  contentPlace.appendChild(link);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const posts = getPosts();
-  console.log(posts);
+async function getPosts() {
+  let pageID = window.location.search;
+
+  if (pageID === '') pageID = 1;
+
+  const response = await fetch(`https://gorest.co.in/public/v2/posts?page=${pageID}`);
+  const data = await response.json();
+
+  data.forEach((el) => {
+    createPosts(el);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await getPosts();
 });

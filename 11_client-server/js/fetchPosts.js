6c314e7f8c86f5bb369c9b35238a61c0e4444103
Denis <kanukov.denis@gmail.com>
2022-09-17 20:44:17 +0300

    manage pagination highlight and creation

--- 11_client-server/js/fetchPosts.js
+++ 11_client-server/js/fetchPosts.js
@@ -1,4 +1,31 @@
 const contentPlace = document.getElementById('content');
+const pagination = document.getElementById('pagination');
+
+function createPagination() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const pageID = urlParams.get('pageID');
+  for (let i = 0; i < 10; i++) {
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.classList.add('page-link');
+    li.classList.add('page-item');
+    link.innerHTML = i + 1;
+    if (i === 0) {
+      link.href = 'postList.html';
+    } else {
+      link.href = `postList.html?pageID=${i + 1}`;
+    }
+    li.append(link);
+    pagination.append(li);
+  }
+  const paginationList = document.querySelectorAll('.page-item');
+  if (pageID === null) {
+    paginationList[0].classList.add('active');
+  } else {
+    paginationList[pageID - 1].classList.add('active');
+    console.log(pageID);
+  }
+}
 
 function createPosts(post) {
   const link = document.createElement('a');
@@ -20,13 +47,19 @@ function createPosts(post) {
 }
 
 async function getPosts() {
-  let pageID = window.location.search;
+  const urlParams = new URLSearchParams(window.location.search);
+  let pageID = urlParams.get('pageID');
 
-  if (pageID === '') pageID = 1;
+  if (pageID === null) {
+    pageID = 1;
+  } else {
+    pageID++;
+  }
 
   const response = await fetch(`https://gorest.co.in/public/v2/posts?page=${pageID}`);
   const data = await response.json();
 
+  console.log(data);
   data.forEach((el) => {
     createPosts(el);
   });
@@ -34,4 +67,8 @@ async function getPosts() {
 
 document.addEventListener('DOMContentLoaded', async () => {
   await getPosts();
+  createPagination();
+  // const response = await fetch(`https://gorest.co.in/public/v2/posts?page=2`);
+  // const data = await response.json();
+  // console.log(data)
 });

5e8d1ffb90173320e5ddbbcbd1f15bbe55b729d8
Denis <kanukov.denis@gmail.com>
2022-09-19 17:38:13 +0300

    add comment generation

--- 11_client-server/js/postRender.js
+++ 11_client-server/js/postRender.js
@@ -1,27 +1,41 @@
+const commentContent = document.getElementById('commentContent');
+
 async function fetchPost(postID) {
   const res = await fetch(`https://gorest.co.in/public-api/posts/${postID}`);
   const data = await res.json();
-  console.log(data.data);
   document.getElementById('title').innerText = data.data.title;
   document.getElementById('body').innerText = data.data.body;
+}
+
+function createComment(comment) {
+  const commentCard = document.createElement('div');
+  const commentAuthor = document.createElement('h5');
+  const commentText = document.createElement('p');
+
+  commentAuthor.innerText = comment.name;
+  commentText.innerText = comment.body;
+  commentCard.append(commentAuthor, commentText);
+  commentContent.append(commentCard);
+}
+
+function displayErrorMessage() {
+  const errorMessage = document.createElement('h4');
+  errorMessage.classList.add('text-danger', 'text-center');
+  errorMessage.innerText = 'У данного поста еще не комментариев';
+  commentContent.append(errorMessage);
 }
 
 async function fetchComments(posID) {
   const res = await fetch(`https://gorest.co.in/public-api/comments?post_id=${posID}`);
   const data = await res.json();
 
-  console.log(data);
   if (!data.data.length) {
-    console.log('no comments')
+    displayErrorMessage();
   } else {
     data.data.forEach((comment) => {
       createComment(comment);
-    })
+    });
   }
-}
-
-function createComment(comment) {
-  console.log('creatingComments');
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
